9a4a3c83d197f4e17885e34a8c1bf7780956cdac
Ivanhahanov <ivanhahanov13@gmail.com>
2021-04-18 21:52:46 +0300

    add check flags routes

--- api/app/routers/challenges.py
+++ api/app/routers/challenges.py
@@ -4,11 +4,15 @@ from datetime import datetime
 import os
 import stat
 import docker
+import tarfile
+import yaml
 
 client = docker.from_env()
 router = APIRouter()
 db = mongo.secure_code_platform
+challenges = db.challenges
 upload_path = '/api/solutions/'
+upload_challenges_folder = '/api/challenges'
 challenges_categories = ['web', 'crypto', 'forensic', 'network', 'linux', 'reverse']
 challenges_tags = []
 challenges_difficult = ['easy', 'medium', 'hard', 'impossible']
@@ -24,14 +28,13 @@ class WriteUp(BaseModel):
 
 
 class Challenge(BaseModel):
+    shortname: str
     title: str
     text: str
-    input_example: str
-    output_example: str
     score: int
     tags: List[str] = []
     category: str
-    author: str = None
+    author: str
     first_blood: str = None
     solutions_num: int = 0
     wrong_solutions_num: int = 0
@@ -40,7 +43,7 @@ class Challenge(BaseModel):
     challenge_created: datetime = None
     challenge_modified: datetime = None
     useful_resources: List[str] = []
-    requirements: List[str] = []
+    flag: str
 
 
 class ContainerChallenge(Challenge):
@@ -64,12 +67,13 @@ class ShortChallenge(BaseModel):
 def get_challenges_list(current_user: User = Depends(get_current_active_user),
                         page_number: int = 1, row_count: int = 10):
     short_challenges = []
-    solved_challenges_id = users.find_one({'username': current_user.username})['solved_challenges_id']
+    solved_challenges_id = users.find_one({'username': current_user.username}).get('solved_challenges_id')
     for challenge in challenges.find({}):
-        if challenge['_id'] in set(map(ObjectId, solved_challenges_id)):
-            short_challenges.append(ShortChallenge(**challenge, solved=True))
+        if solved_challenges_id is not None:
+            if challenge['_id'] in set(map(ObjectId, solved_challenges_id)):
+                short_challenges.append(ShortChallenge(**challenge, solved=True))
         else:
-            short_challenges.append(ShortChallenge(**challenge, solved=True))
+            short_challenges.append(ShortChallenge(**challenge, solved=False))
     return {"challenges": short_challenges[(page_number - 1) * row_count:page_number * row_count]}
 
 
@@ -87,54 +91,103 @@ def category_list(_: User = Depends(get_current_active_user)):
     return {"category_list": challenges_categories}
 
 
-@router.put('/add_container_challenge')
-async def add_container_challenge(challenge: ContainerChallenge,
-                                  current_user: User = Depends(get_current_user_if_editor)):
-    challenge = new_challenge_filter(challenge, current_user)
-    challenges.insert(challenge.dict(by_alias=True))
-    return challenge
+#
+# @router.put('/add_container_challenge')
+# async def add_container_challenge(challenge: ContainerChallenge,
+#                                   current_user: User = Depends(get_current_user_if_editor)):
+#     challenge = new_challenge_filter(challenge, current_user)
+#     challenges.insert(challenge.dict(by_alias=True))
+#     return challenge
 
+
+# @router.put('/add_challenge')
+# async def add_web_challenge(challenge: Challenge, current_user: User = Depends(get_current_user_if_editor)):
+#     challenge = new_challenge_filter(challenge, current_user)
+#     challenges.insert(challenge.dict(by_alias=True))
+#     return challenge
 
 @router.put('/add_challenge')
-async def add_web_challenge(challenge: Challenge, current_user: User = Depends(get_current_user_if_editor)):
-    challenge = new_challenge_filter(challenge, current_user)
-    challenges.insert(challenge.dict(by_alias=True))
-    return challenge
+async def add_challenge(current_user: User = Depends(get_current_user_if_editor)):
+    pass
 
 
-@router.put('/add_files_to_challenge')
-async def add_challenge_with_files(challenge_name: str,
-                                   public_file: UploadFile = File(...),
-                                   checkers_file: UploadFile = File(...),
-                                   _: User = Depends(get_current_user_if_editor)):
-    all_challenges_name = [challenge['title'] for challenge in challenges.find({}, {'_id': False})]
-    if challenge_name not in all_challenges_name:
-        raise HTTPException(status_code=400, detail='Invalid challenge name')
-    return {'challenge_name': challenge_name,
-            'public_filename': public_file.filename,
-            'checkers_file': checkers_file.filename}
+# @router.put('/add_files_to_challenge')
+# async def add_challenge_with_files(challenge_name: str,
+#                                    public_file: UploadFile = File(...),
+#                                    checkers_file: UploadFile = File(...),
+#                                    _: User = Depends(get_current_user_if_editor)):
+#     all_challenges_name = [challenge['title'] for challenge in challenges.find({}, {'_id': False})]
+#     if challenge_name not in all_challenges_name:
+#         raise HTTPException(status_code=400, detail='Invalid challenge name')
+#     return {'challenge_name': challenge_name,
+#             'public_filename': public_file.filename,
+#             'checkers_file': checkers_file.filename}
 
 
-@router.get('/show_task')
-def show_task(current_user: User = Depends(get_current_active_user)):
-    return {'username': current_user.username, 'pwd': list(os.listdir('api/challenges/web/example'))}
+# @router.get('/show_task')
+# def show_task(current_user: User = Depends(get_current_active_user)):
+#     return {'username': current_user.username, 'pwd': list(os.listdir('api/challenges/web/example'))}
 
 
 @router.get('/get_challenge')
-def get_challenge_name(challenge_id: str, _: User = Depends(get_current_active_user)):
-    challenge = Challenge(**challenges.find_one({'_id': ObjectId(challenge_id)}))
+def get_challenges(shortname: str, _: User = Depends(get_current_active_user)):
+    challenge = Challenge(**challenges.find_one({'shortname': shortname}))
     return challenge
 
 
-@router.post("/upload_solution/")
-async def upload_solution(challenge_id: str, current_user: User = Depends(get_current_active_user),
-                          file: UploadFile = File(...)):
-    timestamp = str(datetime.now().timestamp()).replace('.', '')
-    filename = f"{timestamp}_{challenge_id}_{file.filename}"
-    upload_file(filename, file)
-    result, message = await check_container_solution(filename, challenge_id)
-    write_result_to_user_stat(current_user, challenge_id, result)
-    return {'username': current_user.username, 'result': result, 'detail': message}
+@router.post('/upload_challenges')
+def upload_challenges(archive: UploadFile = File(...), current_user: User = Depends(get_current_user_if_editor)):
+    """
+    :param archive: archive.tar with challenges
+    :return: True if success
+    """
+
+    file_object = archive.file
+
+    with tarfile.open(fileobj=file_object, mode='r') as tar:
+        for name in tar.getnames():
+            if name.endswith('.yml'):
+                description_path = name
+        tar.extractall(upload_challenges_folder)
+    description = os.path.join(upload_challenges_folder, description_path)
+    with open(description, 'r') as f:
+        data = yaml.safe_load(f.read())
+        result = add_description_to_database(data, current_user.username)
+        if result is True:
+            return {"result": True}
+        return {"detail": f"Shortname {','.join(result)} already exists"}
+
+
+def add_description_to_database(data, author):
+    challenges_for_database = list()
+    for shortname, values in data.items():
+        print(shortname, values)
+        challenge = Challenge(**values, shortname=shortname, author=author)
+        if check_exists_shortname(challenge.shortname):
+            challenges_for_database.append(challenge)
+
+    if len(challenges_for_database) != len(data.keys()):
+        return set(data.keys()).difference([value.shortname for value in challenges_for_database])
+
+    for challenge in challenges_for_database:
+        challenges.insert(challenge.dict())
+    return True
+
+
+def check_exists_shortname(shortname):
+    if not list(challenges.find({"shortname": shortname})):
+        return True
+
+
+# @router.post("/upload_solution/")
+# async def upload_solution(challenge_id: str, current_user: User = Depends(get_current_active_user),
+#                           file: UploadFile = File(...)):
+#     timestamp = str(datetime.now().timestamp()).replace('.', '')
+#     filename = f"{timestamp}_{challenge_id}_{file.filename}"
+#     upload_file(filename, file)
+#     result, message = await check_container_solution(filename, challenge_id)
+#     write_result_to_user_stat(current_user, challenge_id, result)
+#     return {'username': current_user.username, 'result': result, 'detail': message}
 
 
 def upload_file(filename, file):
