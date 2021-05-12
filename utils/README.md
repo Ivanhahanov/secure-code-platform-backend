607700d02d848d56f2b47a71160ef1c2dd28b94e
Ivanhahanov <ivanhahanov13@gmail.com>
2021-05-12 19:16:50 +0300

    add upload_challenges script

---
+++ utils/README.md
@@ -0,0 +1,12 @@
+# Platform Utils
+
+## Upload Challenges
+Filename: `upload_challenges.py`
+
+Requirements: 
+- `requests`
+
+Usage:
+```
+python3 utils/upload_challenge.py example_challenges -u admin -p JxvmsiUz
+```
--- utils/upload_challenge.py
+++ utils/upload_challenge.py
@@ -5,7 +5,13 @@ import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dir", type=str,
-                    help="display a square of a given number")
+                    help="directory for compress")
+parser.add_argument("-u", "--username", type=str,
+                    help="user with editor or admin credentials")
+parser.add_argument("-p", "--password", type=str,
+                    help="users password")
+parser.add_argument("-U", "--url", type=str,
+                    help="url of platform", default="localhost")
 args = parser.parse_args()
 
 
@@ -22,13 +28,29 @@ def tar_directory():
     exit(1)
 
 
+def get_token():
+    url = f"http://{args.url}/users/token"
+    data = {
+        "username": args.username,
+        "password": args.password,
+    }
+    r = requests.post(url, data)
+    if r.status_code == 200:
+        token = r.json().get('access_token')
+        if token:
+            return token
+    print("Not valid arguments")
+    exit(1)
+
+
 def upload_challenges():
+    token = get_token()
     header = {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYxODc3MzkxMX0.BFfF7Ek-rzoZ1Kbz4HbTsZ2qZw-F6C_31KSA88ccxlE"}
+        "Authorization": f"Bearer {token}"}
     url = "http://localhost/challenges/upload_challenges"
     archive = {"archive": open("challenges.tar", 'rb')}
     r = requests.post(url, headers=header, files=archive)
-    print(r.content)
+    print(*r.json().values())
 
 
 if __name__ == '__main__':
