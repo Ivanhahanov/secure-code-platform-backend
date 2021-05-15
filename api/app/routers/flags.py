234f64940345f7891c138fcac6e576c12f2bb40c
Ivanhahanov <ivanhahanov13@gmail.com>
2021-05-15 16:41:59 +0300

    fix title to shortname

--- api/app/routers/flags.py
+++ api/app/routers/flags.py
@@ -19,9 +19,7 @@ class Challenge(BaseModel):
 
 class SubmittedUser(BaseModel):
     username: str
-    solved_challenges_title: Dict[str, datetime] = {}
-    num_of_solved_challenges: int = None
-    place_in_scoreboard: int = None
+    solved_challenges: Dict[str, str] = {}
     users_score: int = 0
 
 
@@ -33,15 +31,15 @@ def submit_flag(flag: Flag, current_user: User = Depends(get_current_active_user
     return {"flag": False}
 
 
-def check_flag(flag, title):
-    challenge = get_challenge(title)
+def check_flag(flag, shortname):
+    challenge = get_challenge(shortname)
     if challenge:
         if challenge.flag == flag:
             return True
 
 
-def get_challenge(title):
-    challenge = Challenge(**challenges.find_one({"title": title}))
+def get_challenge(shortname):
+    challenge = Challenge(**challenges.find_one({"shortname": shortname}))
     if all(challenge.dict().values()):
         return challenge
 
@@ -49,7 +47,7 @@ def get_challenge(title):
 def add_score(username, challenge_title):
     user = SubmittedUser(**users.find_one({"username": username}))
     challenge = get_challenge(challenge_title)
-    if not user.solved_challenges_title.get(challenge_title):
-        user.solved_challenges_title[challenge_title] = datetime.now()
+    if not user.solved_challenges.get(challenge_title):
+        user.solved_challenges[challenge_title] = datetime.now(timezone.utc).isoformat()
         user.users_score += challenge.score
         users.update_one({'username': username}, {'$set': user.dict(by_alias=True)})
