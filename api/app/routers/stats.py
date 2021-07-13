e8caf31719be690be2189b54f057f08373927dab
Ivan Hahanov <ivanh@naliway.local>
2021-07-13 18:42:43 +0300

    add users and challenges stats route

--- api/app/routers/stats.py
+++ api/app/routers/stats.py
@@ -1,2 +1,11 @@
 # TODO: stats for prometheus
-# TODO: stats for users and tasks
+from fastapi import APIRouter
+from . import *
+
+router = APIRouter()
+
+
+@router.get('/')
+def stats(_: User = Depends(get_current_active_user)):
+    return {'num_of_users': db.users.find({'user_role': {'$ne': 'admin'}}).count(),
+            'num_of_challenges': db.challenges.find().count()}
