f29dd79112ca59a77d793bb7abf05bf7defd7903
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 18:18:39 +0000

    Adding tarfile member sanitization to extractall()

--- api/app/routers/challenges.py
+++ api/app/routers/challenges.py
@@ -209,7 +209,26 @@ def upload_challenges(archive: UploadFile = File(...), current_user: User = Depe
         for name in tar.getnames():
             if name.endswith('.yml'):
                 description_path = name
-        tar.extractall(upload_challenges_folder)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, upload_challenges_folder)
     description = os.path.join(upload_challenges_folder, description_path)
     with open(description, 'r') as f:
         data = yaml.safe_load(f.read())
